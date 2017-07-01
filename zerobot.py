d2b6bf0afcbf0fd2ebb708678a7483a733bdffa4
Chris Purcell <danzig@rocketmail.com>
2017-07-01 18:52:53 -0500

    Adding modules and updating routines

--- zerobot.py
+++ zerobot.py
@@ -22,6 +22,12 @@ AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "!"
 
 # Bot logic
+def do_intro():
+    channel = '#ring_zero'
+    response = "Zerobot connected and running! Type !commands for a list of available commands."
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+
 def handle_command(command, channel):
     """
         Receives commands directed at the bot and determines if they
@@ -56,9 +62,10 @@ def parse_slack_output(slack_rtm_output):
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            if output and 'text' in output and AT_BOT in output['text']:
+            if output and 'text' in output and output['text'].startswith("!"):
             # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
+#                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                return output['text'].lower(), \
                        output['channel']
     return None, None
 
@@ -66,6 +73,7 @@ if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 0.1 # 1/2 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("Zerobot connected and running!")
+        do_intro()
         while True:
             command, channel = parse_slack_output(slack_client.rtm_read())
             if command and channel:
