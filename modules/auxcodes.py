e7ed9fd00a22d76e98baafd082cab0f3e8328666
Chris Purcell <danzig@rocketmail.com>
2017-07-08 21:28:58 -0500

    Updated formatting of auxcodes

--- modules/auxcodes.py
+++ modules/auxcodes.py
@@ -2,15 +2,15 @@
 
 # List of choices
 codes = [
-    "01 Tickets\n",
-    "02 Break\n",
-    "03 Lunch\n",
-    "04 Meeting\n",
-    "05 Assist\n",
-    "06 Training\n",
-    "07 Project\n",
-    "08 Maintenance\n",
-    "09 Chat\n"
+    "01 Tickets",
+    "02 Break",
+    "03 Lunch",
+    "04 Meeting",
+    "05 Assist",
+    "06 Training",
+    "07 Project",
+    "08 Maintenance",
+    "09 Chat"
 ]
 
 # Create function
