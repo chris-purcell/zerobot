d3330bd9521960cfa023bf478aa96e669b0e09c3
Chris Purcell <danzig@rocketmail.com>
2017-07-10 03:30:09 -0500

    Updated insult

--- modules/insult.py
+++ modules/insult.py
@@ -5,35 +5,21 @@ from random import choice
 
 # List of choices
 insults = [
-    "bedswerver",
-    "cumberworld",
-    "fopdoodle",
-    "tosser",
-    "wanker",
-    "daft cow",
-    "barmy",
-    "minger",
-    "muppet",
-    "nutter",
-    "plonker",
-    "arsehole",
-    "knob head",
-    "maggot"
+    "Bedswerver",
+    "Cumberworld",
+    "Fopdoodle",
+    "Tosser",
+    "Wanker",
+    "Daft cow",
+    "Barmy",
+    "Minger",
+    "Muppet",
+    "Nutter",
+    "Plonker",
+    "Knob head",
+    "Maggot"
 ]
 
 # Create function
-def getRandomInsult():
-    return choice(insults)
-
 def insult():
-    text = ''
-    if text and len(text) > 0:
-        insulter = text.split()[0]
-        print "Insulter: " + insulter
-        insultee = text.split()[2]
-        print "Insultee: " + insultee
-        user = text.strip('!insult ')
-        print user
-        return "%s is a total %s" % (user,getRandomInsult())
-    else:
-        return getRandomInsult()
+    return choice(insults) + "!"
