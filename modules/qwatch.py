60ac521af7fb341a3266b7f29227884b5fd7ac75
Chris Purcell <danzig@rocketmail.com>
2017-07-24 19:30:24 -0500

    Updating qwatch user module

--- modules/qwatch.py
+++ modules/qwatch.py
@@ -1,35 +1,29 @@
 #!/usr/bin/env python
 
 # Import required modules
-import os, requests, time, yaml
-from slackclient import SlackClient
+import os, requests
 from bs4 import BeautifulSoup
 
 def qwatch():
-    channel = '#zerobot'
-    while True:
-        data = []
-        source = requests.get("http://qwatch.it.rackspace.com").text
-        soup = BeautifulSoup(source, 'html.parser')
-        table = soup.find('table')
-        x = (len(table.find_all('tr')))
+    data = []
+    source = requests.get("http://qwatch.it.rackspace.com").text
+    soup = BeautifulSoup(source, 'html.parser')
+    table = soup.find('table')
+    x = (len(table.find_all('tr')))
 
-        try:
-            for row in table.find_all('tr')[1:x]:
-                col = row.find_all('td')
-                name = col[0].getText()
-                ticket = 'https://core.rackspace.com/ticket/' + col[1].getText() + "\n"
-                if 'windows' in name:
-                    pass
-                else:
-                    data.append(name.capitalize())
-                    data.append(ticket)
-        except IndexError:
-            pass
-        if data:
-            response = "Pending SLA Violation tickets:\n" + (' '.join('{}'.format(k) for i,k in enumerate(data)))
-            slack_client.api_call("chat.postMessage", channel=channel,
-                                  text=response, as_user=True)
-        else:
-            pass
-
+    try:
+        for row in table.find_all('tr')[1:x]:
+            col = row.find_all('td')
+            name = col[0].getText()
+            ticket = 'https://core.rackspace.com/ticket/' + col[1].getText() + "\n"
+            if 'windows' in name:
+                pass
+            else:
+                data.append(name.capitalize())
+                data.append(ticket)
+    except IndexError:
+        pass
+    if data:
+        return "Pending SLA Violation tickets:\n" + (' '.join('{}'.format(k) for i,k in enumerate(data)))
+    else:
+        pass
