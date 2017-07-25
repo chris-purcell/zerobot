c5eddbe0810112e940262ab2f60747b6fdc6385d
Chris Purcell <danzig@rocketmail.com>
2017-07-25 09:08:20 -0500

    updated ipfinder

--- ipfinder.py
+++ ipfinder.py
@@ -8,10 +8,14 @@ from lxml import html
 def ipfinder():
 # Set up the variables
     IPADDR='192.168.1.100'
-    source = requests.get("https://ipfinder.rackspace.com/" + IPADDR, verify=False).text
+    requests.packages.urllib3.disable_warnings()
+    source = requests.get("https://ipfinder.rackspace.com/"+ IPADDR, verify=False).text
     tree = html.fromstring(source)
     ip_info = tree.xpath('//div[@class="row"]/div/p/text()')
     dev_info = tree.xpath('//div[@class="row"]/div/p/a/text()')
-    return ("IP Address:" + ('%s' % ip_info[0].split(":")[1].split("\n")[0]) + "\n" + "Account: " \
-            + dev_info[0] + "\n" + "Device: " + dev_info[1])
+    return "IP Address:" + ('%s' % ip_info[0].split(":")[1].split("\n")[0]) + "\n" + \
+           "Account: https://core.rackspace.com/py/core/#/account/" + dev_info[0] + "\n" + \
+           "Device: https://core.rackspace.com/py/core/#/device/" + dev_info[1]
+
+ipfinder()
 
